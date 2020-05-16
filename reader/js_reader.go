9e9329835ceeb6f12be36c67cb76c669cc78a380
Kyoungho An <kyoungho.an@gmail.com>
2020-05-16 16:00:24 -0700

    updated

--- reader/js_reader.go
+++ reader/js_reader.go
@@ -1,4 +1,4 @@
-package reader
+package main
 
 import (
 	"github.com/rticommunity/rticonnextdds-connector-go"
--- writer/js_writer.go
+++ writer/js_writer.go
@@ -1,4 +1,4 @@
-package writer
+package main
 
 import (
 	"github.com/rticommunity/rticonnextdds-connector-go"
